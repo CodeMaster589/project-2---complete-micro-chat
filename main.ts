cd1390b95825fa198cc34c1ac34a50cf31aa8251
CodingRules123 <157979538+CodeMaster589@users.noreply.github.com>
2024-02-03 22:10:18 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Hi!")
+    radio.sendString("My name is NAME, What’s your name?")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("PUTWHATEVERYOUWANTHERETOSEND")
+    radio.sendString("PUTWHATEVERYOUWANTHERETOSEND")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("I am good, Thank you.")
+    radio.sendString("What are you doing?")
+})
+radio.setGroup(1)
